4d3b0e5ebcf36a1da1b8eebbb994bf9cabb55496
emonShimoul <emonshimoul@gmail.com>
2026-09-17 18:03:38 +0600

    start working with the history

--- src/App.jsx
+++ src/App.jsx
@@ -68,12 +68,28 @@ export default function Game() {
     setHistory([...history, nextSquares]);
   }
 
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to the move # ${move}`;
+    } else {
+      description = `Go to start the Game!`;
+    }
+    return (
+      <li>
+        <button>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div>
       <div>
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div>{/* TBD */}</div>
+      <div>
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
